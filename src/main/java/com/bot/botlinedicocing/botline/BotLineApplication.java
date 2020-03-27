0e28f90b2268ac78c8f43e5536404e3726d5d2f7
Hani-Loli <33348657+haniueo@users.noreply.github.com>
2020-03-27 14:44:16 +0700

    Update and rename src/main/java/com/bot/botLine/BotLineApplication.java to src/main/java/com/bot/botlinedicocing/botline/BotLineApplication.java

--- src/main/java/com/bot/botlinedicocing/botline/BotLineApplication.java
+++ src/main/java/com/bot/botlinedicocing/botline/BotLineApplication.java
@@ -1,4 +1,4 @@
-package com.botline;
+package com.botlinedicoding.botline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
