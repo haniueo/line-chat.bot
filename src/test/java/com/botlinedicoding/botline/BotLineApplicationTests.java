ad1350126e6662762b5a57022587edeb31a34bb1
Hani-Loli <33348657+haniueo@users.noreply.github.com>
2020-03-27 14:50:30 +0700

    Add files via upload

---
+++ src/test/java/com/botlinedicoding/botline/BotLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botlinedicoding.botline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
